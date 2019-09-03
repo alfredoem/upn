1914b83eb7ddfec38a53fac6b89a3d35fa285fa8
AlfredoEM <alfredo.espiritu.m@gmail.com>
2019-09-02 21:48:26 -0500

    feat(personaApp): solcitar y mostra IMC

---
+++ poo/GuiaLaboratorioNDos/src/main/java/PersonaIMC/PersonaAPP.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PersonaIMC;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author alfredo
+ */
+public class PersonaAPP {
+    
+    public static void main(String[] args) {
+        String nombre = JOptionPane.showInputDialog("Introduce el nombre");
+        
+        String texto = JOptionPane.showInputDialog("Introduce la edad");
+        int edad = Integer.parseInt(texto);
+        
+        texto = JOptionPane.showInputDialog("Introduce el sexo");
+        char sexo = texto.charAt(0);
+        
+        texto = JOptionPane.showInputDialog("Introduce el peso");
+        double peso = Double.parseDouble(texto);
+        
+        texto = JOptionPane.showInputDialog("Introduce la altura");
+        double altura = Double.parseDouble(texto);
+        
+        PersonaIMC persona1 = new PersonaIMC();
+        PersonaIMC persona2 = new PersonaIMC(nombre, edad, sexo);
+        PersonaIMC persona3 = new PersonaIMC(nombre, edad, sexo, peso, altura);
+        
+        persona1.setNombre("Luisa");
+        persona1.setEdad(28);
+        persona1.setSexo('F');
+        persona1.setPeso(60);
+        persona1.setAltura(1.8);
+        
+        persona2.setPeso(80.5);
+        persona2.setAltura(1.75);
+        
+        System.out.println("Persona 1");
+        muestraMensajePeso(persona1);
+        muestraMayorDeEdad(persona1);
+        System.out.println(persona1.toString());
+        
+        System.out.println("Persona 2");
+        muestraMensajePeso(persona2);
+        muestraMayorDeEdad(persona2);
+        System.out.println(persona2.toString());
+        
+        System.out.println("Persona 3");
+        muestraMensajePeso(persona3);
+        muestraMayorDeEdad(persona3);
+        System.out.println(persona3.toString());
+    }
+        
+    public static void muestraMensajePeso(PersonaIMC p) {
+        int IMC = p.calcularIMC();
+
+        switch  (IMC) {
+            case PersonaIMC.PESO_IDEAL:
+                System.out.println("La persona esta en su peso ideal");
+                break;
+            case PersonaIMC.INFRAPESO:
+                System.out.println("La persona esta debajo de su peso ideal");
+                break;
+            case PersonaIMC.SOBREPESO:
+                System.out.println("La persona esta por encima de su peso ideal");
+                break;
+        }
+    }
+
+
+    public static void muestraMayorDeEdad(PersonaIMC p) {
+        if (p.esMayorDeEdad()) {
+            System.out.println("La persona es mayor de edad");
+        } else {
+            System.out.println("La persona no es mayor de edad");
+        }
+    }
+    
+}
--- poo/GuiaLaboratorioNDos/src/main/java/PersonaIMC/PersonaIMC.java
+++ poo/GuiaLaboratorioNDos/src/main/java/PersonaIMC/PersonaIMC.java
@@ -51,13 +51,87 @@ public class PersonaIMC {
     }
     
     private void generarDni() {
+        final int divisor = 23;
+        
+        int numDNI = ((int) Math.floor(Math.random() * (100000000 - 10000000) + 10000000));
+        int res = numDNI - (numDNI / divisor * divisor);
+        
+        char letraDNI = generaLetraDNI(res);
+        
+        DNI = Integer.toString(numDNI) + letraDNI;
         
     }
     
+    private char generaLetraDNI(int res) {
+        char letras[] = {'T', 'R', 'W', 'A', 'G', 'M', 'Y',
+            'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z',
+            'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'
+        };
+        
+        return letras[res];
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
     
     
+    public int calcularIMC() {
+        double pesoActual = peso / (Math.pow(altura, 2));
+        
+        if (pesoActual >= 20 && pesoActual <= 25) {
+            return PESO_IDEAL;
+        } else if (pesoActual < 20) {
+            return INFRAPESO;
+        } else {
+            return SOBREPESO;
+        }
+    }
     
     
-    
-    
+    public boolean esMayorDeEdad() {
+        boolean mayor = false;
+        
+        if (edad >= 18) {
+            mayor = true;
+        }
+        
+        return mayor;
+    }
+
+    @Override
+    public String toString() {
+        String sexo;
+        
+        if (this.sexo == 'M') {
+            sexo = "masculino";
+        } else {
+            sexo = "femenino";
+        }
+        
+        return "Informaci[on de la persona: \n"
+                + "Nombre: " + nombre + "\n"
+                + "Sexo: " + sexo + "\n"
+                + "Edad: " + edad + " a\n"
+                + "DNI: " + DNI + "\n"
+                + "Peso: " + peso + "\n"
+                + "Altura: " + altura + "\n";
+    }
+     
 }
